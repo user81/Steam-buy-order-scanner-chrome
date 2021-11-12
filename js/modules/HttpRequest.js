ec252e379f6e2564d4d0b1777bad8c267f30d589
user81 <ermachenya.al@yandex.by>
2021-11-12 20:06:44 +0300

    First commit

--- js/modules/HttpRequest.js
+++ js/modules/HttpRequest.js
@@ -74,21 +74,4 @@ globalThis.httpPostErrorPause = async function(httpUrl, httpParams, attempts = 8
     });
 };
 
-
 async function waitTime(ms) { return new Promise(resolve => setTimeout(resolve,ms)); }
-
-
-/* var http = new XMLHttpRequest();
-var url = 'get_data.php';
-var params = 'orem=ipsum&name=binny';
-http.open('POST', url, true);
-
-//Send the proper header information along with the request
-http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-
-http.onreadystatechange = function() {//Call a function when the state changes.
-    if(http.readyState == 4 && http.status == 200) {
-        alert(http.responseText);
-    }
-}
-http.send(params); */
